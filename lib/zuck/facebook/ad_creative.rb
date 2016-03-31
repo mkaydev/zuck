2d45bcc1f0e011e72d7406ee789200cbf7434d68
Martin Krueger <martin@playtestcloud.com>
2016-03-31 13:32:38 +0200

    Feature[#PTC-47]: updating ad creative known keys to v2.5

--- lib/zuck/facebook/ad_creative.rb
+++ lib/zuck/facebook/ad_creative.rb
@@ -1,26 +1,38 @@
 module Zuck
   class AdCreative < RawFbObject
-
     # Known keys as per
-    # [fb docs](https://developers.facebook.com/docs/reference/ads-api/adaccount/)
-    known_keys :actor_id,
+    # [fb docs](https://developers.facebook.com/docs/marketing-api/reference/ad-creative/v2.5)
+    known_keys :id,
+               :actor_id,
+               :actor_image_hash,
+               :actor_image_url,
+               :actor_name,
+               :adlabels,
+               :applink_treatment,
                :body,
                :call_to_action_type,
                :image_crops,
                :image_hash,
                :image_url,
+               :instagram_actor_id,
+               :instagram_permalink_url,
+               :link_og_id,
                :link_url,
                :name,
                :object_id,
                :object_story_id,
                :object_story_spec,
+               :object_type,
                :object_url,
+               :platform_customizations,
+               :product_set_id,
+               :run_status,
+               :template_url,
+               :thumbnail_url,
                :title,
-               :url_tags,
-               :id
+               :url_tags
 
     parent_object :ad_group
-    list_path     :adcreatives
-
+    list_path :adcreatives
   end
 end
