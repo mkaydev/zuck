ae906f2dbd675f9c97ddf90fe92cf2d7c6bc52f4
Martin Krueger <martin@playtestcloud.com>
2016-03-31 13:29:27 +0200

    Feature[#PTC-47]: updating ad campaign known keys to v2.5

--- lib/zuck/facebook/ad_campaign.rb
+++ lib/zuck/facebook/ad_campaign.rb
@@ -1,15 +1,26 @@
 module Zuck
   class AdCampaign < RawFbObject
-
+    # Known keys as per
+    # [fb docs](https://developers.facebook.com/docs/marketing-api/reference/ad-campaign-group/v2.5)
     known_keys :id,
                :account_id,
-               :objective,
+               :adlabels,
+               :buying_type,
+               :can_use_spend_cap,
+               :configured_status,
+               :created_time,
+               :effective_status,
                :name,
+               :objective,
+               :recommendations,
+               :spend_cap,
+               :start_time,
                :status,
-               :buying_type
+               :stop_time,
+               :updated_time
 
     parent_object :ad_account, as: :account_id
-    list_path     :campaigns
-    connections   :ad_campaigns
+    list_path :campaigns
+    connections :ad_groups, :ad_campaigns
   end
 end
