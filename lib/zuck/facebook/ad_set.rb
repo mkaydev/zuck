46f0dcf53672710c8384899b034b4c823afc14d9
Martin Krueger <martin@playtestcloud.com>
2016-03-31 12:56:32 +0200

    Feature[#PTC-47]: updating known keys list for ad set to v2.5

--- lib/zuck/facebook/ad_set.rb
+++ lib/zuck/facebook/ad_set.rb
@@ -4,24 +4,42 @@ module Zuck
   class AdSet < RawFbObject
 
     # Known keys as per
-    # [fb docs](https://developers.facebook.com/docs/reference/ads-api/adset/v2.2)
-    known_keys :id,
-               :name,
-               :account_id,
+    # [fb docs](https://developers.facebook.com/docs/reference/ads-api/adset/v2.5)
+    known_keys :account_id,
+               :adlabels,
+               :adset_schedule,
                :bid_amount,
                :bid_info,
+               :billing_event,
+               :budget_remaining,
                :campaign,
                :campaign_id,
-               :status,
-               :start_time,
-               :end_time,
-               :updated_time,
+               :configured_status,
                :created_time,
+               :creative_sequence,
                :daily_budget,
+               :effective_status,
+               :end_time,
+               :frequency_cap_reset_period,
+               :frequency_control_specs,
+               :id,
+               :frequency_cap,
+               :is_autobid,
                :lifetime_budget,
-               :budget_remaining,
+               :lifetime_frequency_cap,
+               :lifetime_imps,
+               :name,
+               :optimization_goal,
+               :pacing_type,
+               :product_ad_behavior,
+               :promoted_object,
+               :recommendations,
+               :rf_prediction_id,
+               :rtb_flag,
+               :start_time,
+               :status,
                :targeting,
-               :promoted_object
+               :updated_time
 
     parent_object :ad_account, as: :account_id
     list_path     :adsets # Yes, this is correct, "for legacy reasons"
